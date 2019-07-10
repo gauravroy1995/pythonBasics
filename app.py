3ec37bb720da9c16f96138974cc49b317701bd95
gaurav roy <rockingroy2009@gmail.com>
2019-07-10 08:33:38 +0530

    inhertiance

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import random
 
 
-class Dice:
+# class Dice:
+#
+#     pi = 3
+#
+#     def __init__(self,numbers):
+#         self.numbers = numbers
+#
+#     # def roll(self):
+#     #      print(f"({random.choice(self.numbers)},{random.choice(self.numbers)})")
+#     def area(self):
+#         return self.numbers
 
-    pi = 3
 
-    def __init__(self,numbers):
-        self.numbers = numbers
+class Dog():
 
-    # def roll(self):
-    #      print(f"({random.choice(self.numbers)},{random.choice(self.numbers)})")
-    def area(self):
-        return self.numbers
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print(self.name)
+
+
+class Cat(Dog):
+    def __init__(self):
+        Dog.__init__(self, 'aditi')
+
+
+cat = Cat()
+print(cat.print_name())
