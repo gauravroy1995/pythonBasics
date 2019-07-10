107375859b5372eda7609d3f8c527c892b9119ea
gaurav roy <rockingroy2009@gmail.com>
2019-07-10 08:18:06 +0530

    excersise

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import random
 
 class Dice:
 
+    pi = 3
+
     def __init__(self,numbers):
         self.numbers = numbers
 
-    def roll(self):
-         print(f"({random.choice(self.numbers)},{random.choice(self.numbers)})")
-
+    # def roll(self):
+    #      print(f"({random.choice(self.numbers)},{random.choice(self.numbers)})")
+    def area(self):
+        return self.numbers
